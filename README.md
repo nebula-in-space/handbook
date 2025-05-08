236cadb443247b2ce2fae310e03284e0d680e508
KOCMOHABT <KOCMODECAHTHUK@inbox.ru>
2025-05-08 17:26:09 +0500

    Несмешная нарезка CL add: Новый логотип на фоне qol: Переименование в /TG/shiptest del: Руководства: Все ролей антагонистов, и много чего прочего. /CL

--- README.md
+++ README.md
@@ -1,31 +1,20 @@
-# /tg/station handbook
+# /TG/shiptest handbook
 
-[![Publish on GitHub Pages](https://github.com/Hamcha/tghandbook/actions/workflows/publish.yml/badge.svg)](https://github.com/Hamcha/tghandbook/actions/workflows/publish.yml) [![Lint](https://github.com/Hamcha/tghandbook/actions/workflows/lint.yml/badge.svg)](https://github.com/Hamcha/tghandbook/actions/workflows/lint.yml)
+[![Publish on GitHub Pages](https://github.com/nebula-in-space/handbook/actions/workflows/publish.yml/badge.svg)](https://github.com/nebula-in-space/handbook/actions/workflows/publish.yml) [![Lint](https://github.com/nebula-in-space/handbook/actions/workflows/lint.yml/badge.svg)](https://github.com/nebula-in-space/handbook/actions/workflows/lint.yml)
 
-A fancier way to browse the /tg/ wiki. This handbook is a single page application that bundles 20+ pages from the /tg/station wiki, adds some fancy filtering and Nanotrasen styling.
+Более удобный способ просмотра **/TG/shiptest wiki**. Это техническая вики где описаны. 
+Руководства представляют собой одностраничное приложение, которое объединяет более 20 страниц из **/TG/shiptest wiki**, добавляет несколько необычных фильтров, и своеобраную черную тему.
 
-Try it live here: https://hamcha.github.io/tghandbook/
+Пробовать тут: https://nebula-in-space.github.io/handbook/
 
-## What does it do
+## Вопросы?
 
-It adapts the pages layout to be narrow as it's meant to be used in a small window, much like a PDA or an in-game book.
-
-The initial focus was on Chemistry, but the handbook is also good if you need to cross-reference multiple guides at the same time thanks to its fuzzy-search functionality, plus it's gonna get better support for other guides _eventually_.
-
-## I found a problem
-
-Cool! Please open an issue about it [here](https://github.com/Hamcha/tghandbook/issues) or alternatively contact me (@Hamcha, I'm on /tg/station's discord server)
-
-## Development
-
-Start hot-reloading server with `npm start`
-
-Build with `npm run build`
-
-Note: While in development mode some compile-time variables (e.g. the changelog data) will use a locally downloaded version (not up to date) to avoid making requests to GitHub API (as the quota is really low).
+Писать сюда discord: @KOCMOHABT
 
 ## Credits & License
 
-The project is licensed under AGPL-v3.0-only (SPDX identifier). Please see the LICENSE file for details.
+Проект лицензирован только по AGPL-v3.0-only (SPDX identifier).
 
-This project ships exported pages from the /tg/station wiki in `./data/pages`. Those pages have been written by tons of contributors, check the user list here: https://tgstation13.org/wiki/Special:ListUsers
+Этот проект загружает собственные страницы, основанных **/TG/station**
+
+Эти страницы были написаны множеством авторов, полный список: https://tgstation13.org/wiki/Special:ListUsers
--- src/ui/sections.ts
+++ src/ui/sections.ts
@@ -4,10 +4,10 @@ import medicine from "@assets/images/tab-icons/medicine.svg";
 import plumbing from "@assets/images/tab-icons/plumbing.svg";
 import grenade from "@assets/images/tab-icons/grenade.svg";
 import genetics from "@assets/images/tab-icons/genetics.svg";
-import virus from "@assets/images/tab-icons/virus.svg";
+//import virus from "@assets/images/tab-icons/virus.svg";
 import surgery from "@assets/images/tab-icons/surgery.svg";
 import trauma from "@assets/images/tab-icons/trauma.svg";
-import wound from "@assets/images/tab-icons/wound.svg";
+//import wound from "@assets/images/tab-icons/wound.svg";
 import ghetto from "@assets/images/tab-icons/ghetto.svg";
 import construction from "@assets/images/tab-icons/construction.svg";
 import machines from "@assets/images/tab-icons/machines.svg";
@@ -18,35 +18,35 @@ import turbine from "@assets/images/tab-icons/turbine.svg";
 import atmos from "@assets/images/tab-icons/atmos.svg";
 import tcomm from "@assets/images/tab-icons/tcomm.svg";
 import rnd from "@assets/images/tab-icons/rnd.svg";
-import toxins from "@assets/images/tab-icons/toxins.svg";
-import xeno from "@assets/images/tab-icons/xeno.svg";
+//import toxins from "@assets/images/tab-icons/toxins.svg";
+//import xeno from "@assets/images/tab-icons/xeno.svg";
 import rules from "@assets/images/tab-icons/rules.svg";
 import aimod from "@assets/images/tab-icons/aimod.svg";
 import critter from "@assets/images/tab-icons/critter.svg";
 import races from "@assets/images/tab-icons/races.svg";
 import food from "@assets/images/tab-icons/food.svg";
 import hydro from "@assets/images/tab-icons/hydro.svg";
-import song from "@assets/images/tab-icons/song.svg";
+//import song from "@assets/images/tab-icons/song.svg";
 import crate from "@assets/images/tab-icons/crate.svg";
-import space from "@assets/images/tab-icons/space.svg";
+//import space from "@assets/images/tab-icons/space.svg";
 //import aux from "@assets/images/tab-icons/auxbase.svg";
 import robo from "@assets/images/tab-icons/robo.svg";
 import security from "@assets/images/tab-icons/security.svg";
 import law from "@assets/images/tab-icons/law.svg";
 import sop from "@assets/images/tab-icons/sop.svg";
-import trial from "@assets/images/tab-icons/trial.svg";
-import traitor from "@assets/images/tab-icons/traitor.svg";
+//import trial from "@assets/images/tab-icons/trial.svg";
+//import traitor from "@assets/images/tab-icons/traitor.svg";
 import hacking from "@assets/images/tab-icons/hacking.svg";
 import weapons from "@assets/images/tab-icons/weapons.svg";
 import uplink from "@assets/images/tab-icons/uplink.svg";
-import rev from "@assets/images/tab-icons/rev.svg";
-import cult from "@assets/images/tab-icons/cult.svg";
-import nuke from "@assets/images/tab-icons/nuke.svg";
-import malf from "@assets/images/tab-icons/malf.svg";
+//import rev from "@assets/images/tab-icons/rev.svg";
+//import cult from "@assets/images/tab-icons/cult.svg";
+//import nuke from "@assets/images/tab-icons/nuke.svg";
+//import malf from "@assets/images/tab-icons/malf.svg";
 import combat from "@assets/images/tab-icons/combat.svg";
 import access from "@assets/images/tab-icons/access.svg";
-import xmorph from "@assets/images/tab-icons/xmorph.svg";
-import abduction from "@assets/images/tab-icons/abduction.svg";
+//import xmorph from "@assets/images/tab-icons/xmorph.svg";
+//import abduction from "@assets/images/tab-icons/abduction.svg";
 //import cyto from "@assets/images/tab-icons/cyto.svg";
 //import circuits from "@assets/images/tab-icons/circuit.svg";
 //import tourist from "@assets/images/tab-icons/tourist.svg";
@@ -57,10 +57,10 @@ import Guide_to_medicine from "@pages/Guide_to_medicine.json";
 import Guide_to_chemistry from "@pages/Guide_to_chemistry.json";
 import Guide_to_plumbing from "@pages/Guide_to_plumbing.json";
 import Grenade from "@pages/Grenade.json";
-import Infections from "@pages/Infections.json";
+//import Infections from "@pages/Infections.json";
 import Surgery from "@pages/Surgery.json";
 import Guide_to_Traumas from "@pages/Guide_to_Traumas.json";
-import Guide_to_Wounds from "@pages/Guide_to_Wounds.json";
+//import Guide_to_Wounds from "@pages/Guide_to_Wounds.json";
 import Guide_to_Ghetto_Chemistry from "@pages/Guide_to_Ghetto_Chemistry.json";
 import Guide_to_construction from "@pages/Guide_to_construction.json";
 import Machines from "@pages/Machines.json";
@@ -73,37 +73,21 @@ import Guide_to_Atmospherics from "@pages/Guide_to_Atmospherics.json";
 import Guide_to_Telecommunications from "@pages/Guide_to_Telecommunications.json";
 import Guide_to_Research_and_Development from "@pages/Guide_to_Research_and_Development.json";
 import Guide_to_robotics from "@pages/Guide_to_robotics.json";
-import Guide_to_toxins from "@pages/Guide_to_toxins.json";
-import Guide_to_xenobiology from "@pages/Guide_to_xenobiology.json";
 import Guide_to_genetics from "@pages/Guide_to_genetics.json";
 import Guide_to_food from "@pages/Guide_to_food.json";
 import Guide_to_drinks from "@pages/Guide_to_drinks.json";
 import Guide_to_hydroponics from "@pages/Guide_to_hydroponics.json";
 import Supply_crates from "@pages/Supply_crates.json";
-import Guide_to_security from "@pages/Guide_to_security.json";
-import Space_Law from "@pages/Space_Law.json";
-import Standard_Operating_Procedure from "@pages/Standard_Operating_Procedure.json";
-import Guide_to_trials from "@pages/Guide_to_trials.json";
 import Guide_to_Combat from "@pages/Guide_to_Combat.json";
-import Traitor from "@pages/Traitor.json";
 import Makeshift_weapons from "@pages/Makeshift_weapons.json";
 import Syndicate_Items from "@pages/Syndicate_Items.json";
 import Illicit_Access from "@pages/Illicit_Access.json";
-import Revolutionary from "@pages/Revolutionary.json";
-import Blood_Cult from "@pages/Blood_Cult.json";
-import Nuclear_Operative from "@pages/Nuclear_Operative.json";
-import Guide_to_malfunction from "@pages/Guide_to_malfunction.json";
-import Xenos from "@pages/Xenos.json";
-import Abductor from "@pages/Abductor.json";
-import Heretic from "@pages/Heretic.json";
 import Rules from "@pages/Rules.json";
 import AI_modules from "@pages/AI_modules.json";
 import Hacking from "@pages/Hacking.json";
 import Critters from "@pages/Critters.json";
 import Guide_to_races from "@pages/Guide_to_races.json";
 import Paper_Markdown from "@pages/Paper_Markdown.json";
-import Songs from "@pages/Songs.json";
-import Guide_to_Space_Exploration from "@pages/Guide_to_Space_Exploration.json";
 
 export interface SectionInfo {
   name: string;
@@ -123,98 +107,80 @@ const sections: SectionInfo[] = [
   {
     name: "Медицина",
     tabs: [
-      { page: "Guide_to_medicine", text: "Медицина", icon: medicine, data: Guide_to_medicine },
-      { page: "Guide_to_chemistry", text: "Химия", icon: chemistry, data: Guide_to_chemistry },
-      { page: "Guide_to_plumbing", text: "Пламбинг", icon: plumbing, data: Guide_to_plumbing },
-      { page: "Grenade", text: "Гранаты", icon: grenade, data: Grenade },
-      { page: "Infections", text: "Вирусы", icon: virus, data: Infections },
-      { page: "Surgery", text: "Хирургия", icon: surgery, data: Surgery },
-      { page: "Guide_to_Traumas", text: "Травмы", icon: trauma, data: Guide_to_Traumas},
-      { page: "Guide_to_Wounds", text: "Ранения", icon: wound, data: Guide_to_Wounds},
-      { page: "Guide_to_Ghetto_Chemistry", text: "Гетто", icon: ghetto, data: Guide_to_Ghetto_Chemistry},
+      { page: "Гайд_на_медицину", text: "Медицина", icon: medicine, data: Guide_to_medicine },
+      { page: "Гайд_на_химию", text: "Химия", icon: chemistry, data: Guide_to_chemistry },
+      { page: "Гайд_на_гетто_химию", text: "Гетто", icon: ghetto, data: Guide_to_Ghetto_Chemistry},
+      { page: "Гайд_на_пламбинг", text: "Пламбинг", icon: plumbing, data: Guide_to_plumbing },
+      { page: "Гранаты", text: "Гранаты", icon: grenade, data: Grenade },
+      { page: "Хирургия", text: "Хирургия", icon: surgery, data: Surgery },
+      { page: "Гайд_на_травмы", text: "Травмы", icon: trauma, data: Guide_to_Traumas},
     ],
   },
   {
     name: "Инженерия",
     tabs: [
-      { page: "Guide_to_construction", text: "Строительство", icon: construction, data: Guide_to_construction},
-      { page: "Machines", text: "Машины", icon: machines, data: Machines },
-      { page: "Guide_to_power", text: "Энергия", icon: power, data: Guide_to_power},
-      { page: "Solars", text: "Панели", icon: solar, data: Solars },
-      { page: "Guide_to_the_Supermatter", text: "Материя", icon: supermatter, data: Guide_to_the_Supermatter },
-      { page: "Guide_to_disposals", text: "Трубы", icon: null, data: Guide_to_disposals }, // TODO: add icon data: Guide_to_disposals
-      { page: "Gas_turbine", text: "Турбина", icon: turbine, data: Gas_turbine },
-      { page: "Guide_to_Atmospherics", text: "Атмос", icon: atmos, data: Guide_to_Atmospherics },
-      { page: "Guide_to_Telecommunications", text: "Телекомы", icon: tcomm, data: Guide_to_Telecommunications },
+      { page: "Гайд_на_Строительство", text: "Строительство", icon: construction, data: Guide_to_construction},
+      { page: "Машины", text: "Машины", icon: machines, data: Machines },
+      { page: "Гайд_на_Энергию", text: "Энергия", icon: power, data: Guide_to_power},
+      { page: "Солнечные панели", text: "Панели", icon: solar, data: Solars },
+      { page: "Гайд_на_Супер_Материю", text: "Материя", icon: supermatter, data: Guide_to_the_Supermatter },
+      { page: "Гайд_на_Турбину", text: "Турбина", icon: turbine, data: Gas_turbine },
+      { page: "Гайд_на_Атмос", text: "Атмос", icon: atmos, data: Guide_to_Atmospherics },
+      { page: "Гайд_на_Трубы", text: "Трубы", icon: null, data: Guide_to_disposals }, // TODO: add icon data: Guide_to_disposals
+      { page: "Гайд_на_Телекоммуникации", text: "Телекомы", icon: tcomm, data: Guide_to_Telecommunications },
     ],
   },
+  //{
+  //  name: "Наука",
+  //  tabs: [
+  //    //{ page: "Гайд_на_Токсины", text: "Токсиы", icon: toxins, data: Guide_to_toxins},
+  //    //{ page: "Гайд_на_Ксенобиологию", text: "Ксено", icon: xeno, data: Guide_to_xenobiology},
+  //    // { page: "Гайд_на_нанитов", text: "nanite", icon: nanites },
+  //  ],
+  //},
   {
-    name: "Наука",
+    name: "Исследование",
     tabs: [
-      { page: "Guide_to_Research_and_Development", text: "R&D", icon: rnd, data: Guide_to_Research_and_Development},
-      { page: "Guide_to_robotics", text: "Роботика", icon: robo, data: Guide_to_robotics },
-      { page: "Guide_to_toxins", text: "Токсиы", icon: toxins, data: Guide_to_toxins},
-      { page: "Guide_to_xenobiology", text: "Ксено", icon: xeno, data: Guide_to_xenobiology},
-      { page: "Guide_to_genetics", text: "Генетика", icon: genetics, data: Guide_to_genetics },
-      // { page: "Guide_to_Nanites", text: "nanite", icon: nanites },
+      { page: "Гайд_на_Исследование_и_Разработку", text: "R&D", icon: rnd, data: Guide_to_Research_and_Development},
+      { page: "Гайд_на_Робототехнику", text: "Роботика", icon: robo, data: Guide_to_robotics },
+      { page: "Модули_ИИ", text: "Модули ИИ", icon: aimod, data: AI_modules },
+      { page: "Гайд_на_Генетику", text: "Генетика", icon: genetics, data: Guide_to_genetics },
+      { page: "Гайд_на_Еду", text: "Еда", icon: food, data: Guide_to_food },
+      { page: "Гайд_на_Напитки", text: "Напитки", icon: drink, data: Guide_to_drinks},
+      { page: "Гайд_на_Гидропонику", text: "Гидропоника", icon: hydro, data: Guide_to_hydroponics },
+      { page: "Ящики", text: "Карго Ящики", icon: crate, data: Supply_crates },
     ],
   },
+  //{
+  //  name: "Охрана",
+  //  tabs: [
+  //    { page: "Гайд_на_Охрану", text: "Охрана", icon: security, data: Guide_to_security},
+  //    { page: "Законы", text: "Законы", icon: law, data: Space_Law },
+  //    { page: "Стандартные_Оперативные_Процедуры", text: "С.О.П.", icon: sop, data: Standard_Operating_Procedure},
+  //    { page: "Суды", text: "Суды", icon: trial, data: Guide_to_trials},
+  //  ],
+  //},
   {
-    name: "Сервис",
+    name: "Разное",
     tabs: [
-      { page: "Guide_to_food", text: "Еда", icon: food, data: Guide_to_food },
-      { page: "Guide_to_drinks", text: "Напитки", icon: drink, data: Guide_to_drinks},
-      { page: "Guide_to_hydroponics", text: "Гидропоника", icon: hydro, data: Guide_to_hydroponics },
-      { page: "Supply_crates", text: "Карго Ящики", icon: crate, data: Supply_crates },
-    ],
-  },
-  {
-    name: "Охрана",
-    tabs: [
-      { page: "Guide_to_security", text: "Охрана", icon: security, data: Guide_to_security},
-      { page: "Space_Law", text: "Законы", icon: law, data: Space_Law },
-      { page: "Standard_Operating_Procedure", text: "С.О.П.", icon: sop, data: Standard_Operating_Procedure},
-      { page: "Guide_to_trials", text: "Суды", icon: trial, data: Guide_to_trials},
-      { page: "Guide_to_Combat", text: "Боевка", icon: combat, data: Guide_to_Combat },
-    ],
-  },
-  {
-    name: "Антаги",
-    tabs: [
-      { page: "Traitor", text: "Предатель", icon: traitor, data: Traitor },
-      { page: "Makeshift_weapons", text: "Самоделки", icon: weapons, data: Makeshift_weapons },
-      { page: "Syndicate_Items", text: "Аплинк", icon: uplink, data: Syndicate_Items},
-      { page: "Illicit_Access", text: "Проникновение", icon: access, data: Illicit_Access },
-      { page: "Revolutionary",text: "Революция", icon: rev, data: Revolutionary },
-      { page: "Blood_Cult", text: "Культ", icon: cult, data: Blood_Cult },
-      { page: "Nuclear_Operative", text: "Нюка", icon: nuke, data: Nuclear_Operative},
-      { page: "Guide_to_malfunction", text: "Малф", icon: malf, data: Guide_to_malfunction },
-      { page: "Xenos", text: "Ксеносы", icon: xmorph, data: Xenos },
-      { page: "Abductor", text: "Абдукторы", icon: abduction, data: Abductor },
-      { page: "Heretic", text: "Еретик", icon: null, data: Heretic },
-    ],
-  },
-  {
-    name: "Другое",
-    tabs: [
-      { page: "Rules", text: "Правила", icon: rules, data: Rules },
-      { page: "AI_modules", text: "Модули ИИ", icon: aimod, data: AI_modules },
-      /*
-      { page: "Guide_to_Awesome_Miscellaneous_Stuff", text: "misc", icon: tips},
-      */
-      { page: "Hacking", text: "Взлом", icon: hacking, data: Hacking },
-      { page: "Critters", text: "Существа", icon: critter, data: Critters },
-      { page: "Guide_to_races", text: "Мутанты", icon: races, data: Guide_to_races },
-      { page: "Paper_Markdown", text: "Документы", icon: null, data: Paper_Markdown },
-      { page: "Songs", text: "Музыка", icon: song, data: Songs },
-      { page: "Guide_to_Space_Exploration", text: "Иследование", icon: space, data: Guide_to_Space_Exploration},
+      { page: "Правила", text: "Правила", icon: rules, data: Rules },
+      { page: "Взлом", text: "Взлом", icon: hacking, data: Hacking },
+      { page: "Существа", text: "Существа", icon: critter, data: Critters },
+      { page: "Гайд_на_расы", text: "Мутанты", icon: races, data: Guide_to_races },
+      { page: "Бумажная_работа", text: "Документы", icon: law, data: Paper_Markdown },
+      //{ page: "Музыка", text: "Музыка", icon: song, data: Songs },
+      //{ page: "Иследование", text: "Иследование", icon: space, data: Guide_to_Space_Exploration},
+      { page: "Самодельное_оружие", text: "Самоделки", icon: weapons, data: Makeshift_weapons },
+      { page: "Предметы_Синдиката", text: "Аплинк", icon: uplink, data: Syndicate_Items},
+      { page: "Незаконный_Доступ", text: "Проникновение", icon: access, data: Illicit_Access },
+      { page: "Боевка", text: "Боевка", icon: combat, data: Guide_to_Combat },
     ],
   },
   {
     name: META,
     tabs: [
-      { page: "$Welcome", text: "HOME", icon: null },
-      { page: "$Changelog", text: "LOG", icon: null },
+      { page: "$Welcome", text: "На главную", icon: null },
+      { page: "$Changelog", text: "Список изменений", icon: null },
     ],
   },
 ];
